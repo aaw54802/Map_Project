9147e347f7c6af5e33f810f374d9e180f5fbbced
aaw54802 <aaw54802@email.vccs.edu>
2024-11-26 18:35:23 -0500

    Add project files.

---
+++ Map_Project.cpp
@@ -0,0 +1,8 @@
+// Map_Project.cpp : This file contains the 'main' function. The classes are scheduleItem, schedule, the csv file, and the client program
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Map project for CSC\n";
+}
